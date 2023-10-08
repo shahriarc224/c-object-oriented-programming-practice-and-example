7bc4ccf397006b63317244e4d99adfd6fd08eec3
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2023-10-08 23:40:32 +0600

    Update inheritance .cpp

--- inheritance .cpp
+++ inheritance .cpp
@@ -1,6 +1,6 @@
 /*inheritance has Derived class and a base class.derived class(child class ),Base class (parents class).
 
-there are 5 ttypes of inheritance 
+there are 5 types of inheritance 
 1.Single Inheritance
 2.Multiple Inheritance
 3.multi lavel Inheritance 
