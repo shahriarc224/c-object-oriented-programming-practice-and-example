ff2975f0ccbfb22a6b87b6149ecb2a2ffde61363
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2023-10-10 19:28:30 +0600

    Create array in object(example).cpp

---
+++ array in object(example).cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class employee
+{
+
+    int id;
+    string name;
+
+public:
+    void getdata()
+    {
+        cout << "Enter Data:" << endl;
+        cin >> id >> name;
+    }
+    void putdata()
+    {
+
+        cout << " Employee id and name : " << id << "  " << name << endl;
+    }
+};
+
+int main()               
+{
+    employee ob[10];        //array in a objct 
+    int i, no;
+    cout << "How many employee" << endl;
+    cin >> no;
+
+    for (i = 0; i < no; i++)
+    {
+        ob[i].getdata();
+    }
+
+    for (i = 0; i < no; i++)
+    {
+        ob[i].putdata();
+    }
+    return 0;
+}
