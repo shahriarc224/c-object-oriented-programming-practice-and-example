c768204eb295c9c8cfa748b14786576e4844a239
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2023-10-11 21:09:00 +0600

    Update unary operator overloding .cpp

--- unary operator overloding .cpp
+++ unary operator overloding .cpp
@@ -42,3 +42,53 @@ int main()
     return 0;
 
 }
+
+//------------------------------------------------------------------------------------------------------------------------------
+
+/*Example of unary operator overloding using class functiuon and friend function.*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class demo{
+    int a,b;
+public:
+    demo(int x,int y)
+    {
+        a=x;
+        b=y;
+    }
+    void show ()
+    {
+
+        cout<<"A="<<a<<endl<<"B="<<b<<endl;
+
+    }
+
+    friend void operator -(demo &obj);  /*First we are going to write(friend) and after that we are 
+                                         going to write the name of the function which we are going
+      };                                 to make frind with the classs */};
+
+
+void operator -(demo &obj )
+{
+
+    obj.a=-obj.a;
+    obj.b=-obj.b;
+
+}
+
+int main()
+{
+    demo ob(10,20);
+    ob.show();
+    -ob;
+    ob.show();
+
+    return 0;
+
+}
+
+
+
+
