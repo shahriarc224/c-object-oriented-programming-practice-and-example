c5642f53c070b8557a6c3e7dab1e91d8cd857b00
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2023-11-04 19:02:52 +0600

    Create constractor.cpp

---
+++ constractor.cpp
@@ -0,0 +1,98 @@
+/*
+Constractor:A constractopr is a special member function of class which is called when an object
+            of that class is created .It is used to initialize the object s data members.
+
+There are three types of constractor ......
+1)Default.  2)Perameterized  3)Copy constractor
+
+Default constractor:Default constractoir is a type of constractor which does not take
+                     any arguments .It does not have any argumenyts .
+
+Parameterized Constractor:A constractor which has perameters is called peramterized
+                         constractor.this constractor uis uesd toprovide different  
+                          value to distinct object.  
+
+
+Copy constractotr:A copy constractor is a special type of constractor in c++ that is used to
+                  create a new object that is an exact copy of an existing object .The copy constractor
+                  is called automatatically when an object ics created as a copy of another
+                  object.
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
