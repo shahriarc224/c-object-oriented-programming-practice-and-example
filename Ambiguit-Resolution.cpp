aa4ddcb425b947b02d24a2333e9ec3dad7198532
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2023-11-16 20:37:43 +0600

    Update Ambiguit-Resolution.cpp

--- Ambiguit-Resolution.cpp
+++ Ambiguit-Resolution.cpp
@@ -1,12 +1,9 @@
 /*
-inheritance has Derived class and a base class.derived class(child class ),Base class (parents class).
+                                                                                                              Youtube:Codeitup
+                                                                                                              link:https://youtu.be/4lTLMxqjhgE?si=SXk4HUnWQ_kxGx72
+Ambiguit-Resolution:
 
-there are 5 types of inheritance 
-1.Single Inheritance
-2.Multiple Inheritance
-3.multi lavel Inheritance 
-4.Hybrid Inheritance
-5.Hierarchical Inheritance 
-
-Example of single inheritance 
 */
+                                                                                                              
+                                                                                                              
+                                                                                                     
