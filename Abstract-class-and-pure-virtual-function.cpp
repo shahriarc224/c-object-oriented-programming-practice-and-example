13c7593845db5edb02b6c7318af1d325eafca493
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2023-11-20 11:19:23 +0600

    Update Abstract-class-and-pure-virtual-function.cpp

--- Abstract-class-and-pure-virtual-function.cpp
+++ Abstract-class-and-pure-virtual-function.cpp
@@ -1,1 +1,26 @@
 /* */
+
+#include<bits/stdc++.h>
+using namespace std;
+ int main(){
+ try{
+ int x,y;
+ cin>>x>>y;
+ if(y<0){
+
+    throw 0;   //We can take any number or character
+
+ }
+ cout<<"sum:"<<x+y<<endl;
+
+ }
+
+ catch (...)  /*If we threw an intager numbre then we need to write int x 
+                 If we threw a character  then we need to write char a
+                 And if we use this  (...)  we cam throw data type. 
+                                                                       */
+   {
+ cout<<"Y is less the zero";
+ }
+
+ }
