44090b095ba7b381e8eb9605db90b1649b5c7c49
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2023-11-20 10:07:01 +0600

    Update Ambiguit-Resolution.cpp

--- Ambiguit-Resolution.cpp
+++ Ambiguit-Resolution.cpp
@@ -8,10 +8,116 @@
 */
 
 
+#include<iostream>
+using namespace std;
+class A{
+    protected:
+    int a;
+    public:
+    void input(){
+        cout<<"\nEnter  value:";
+        cin>>a;
+    }
+    void output(){
+        cout<<"\n A="<<a;
+    }
+    
+};
+class B{
+    
+  protected:
+    int b;
+    public:
+    void input(){
+        cout<<"\nEnter  value:";
+        cin>>b;
+    }
+    void putdata(){
+        cout<<"\n B="<<b;
+    }   
+    };
+    
+    class C:public A,public B  
+    {
+        int c;
+        public:
+        void input(){
+            cout<<"\nEnter value:";
+            cin>>c;
+            A::input(); //class A function input is connected
+            B::input(); //class B function input is connected
+            }
+        void display(){
+            
+            cout<<"\n c="<<c;
+              }
+     };
+
+int main(){
+    C ob;        //make an object of Derived class.
+    ob.input();  //call the function of the derived class
+    ob.display();  
+    ob.output(); //call the function of A
+    ob.putdata(); //call the function of B
+    }
 
 
+// 2nd 
 
+#include<iostream>
+using namespace std;
+class A{
+    protected:
+    int a;
+    public:
+    void input(){
+        cout<<"\nEnter  value:";
+        cin>>a;
+    }
+    void output(){
+        cout<<"\n A="<<a;
+    }
+    
+};
+class B{
+    
+  protected:
+    int b;
+    public:
+    void input(){
+        cout<<"\nEnter  value:";
+        cin>>b;
+    }
+    void putdata(){
+        cout<<"\n B="<<b;
+    }   
+    };
+    
+    class C:public A,public B  
+    {
+        int c;
+        public:
+        void getdata()   /* the function name  is different .So in this case we ned to connect the other classes 
+                         function with this function nad then call the function main funtion */
+            {
+            cout<<"\nEnter value:";
+            cin>>c;
+            A::input(); //class A function input is connected
+            B::input();  //class B function input is connected
+            }
+        void display(){
+            
+            cout<<"\n c="<<c;
+              }
+     };
 
+int main(){
+    C ob;        //make an object of Derived class.
+    ob.getdata();  //call the function of the derived class
+    ob.display();  
+    ob.output(); //call the function of A
+    ob.putdata(); //call the function of B
+    }
 
 
 
