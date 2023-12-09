9cfdd8906cdd11b877947ca3552a90490593b6e2
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2023-12-09 18:54:48 +0600

    Create file.cpp

---
+++ file.cpp
@@ -0,0 +1,62 @@
+#include<iostraem>
+#include<fstream>   /*to read and write in to a file we need    <fstream> library.
+                       */
+using namespace std;
+int main (){
+    
+    
+    ofstream file;     /*ofstraem is used to create and  write information to file */
+    file.open("student.txt");
+    
+    file<<"I am Shahriar.I am 22 years old .Iam from Dhaka.";
+    
+    file.close();
+    
+    return 0;
+}
+
+//..................................................................................................................................................
+
+
+  #include<iostraem>
+#include<fstream>         /*to read and write in to a file we need  <fstream> library.
+                                     */
+using namespace std;
+int main (){
+    
+    string name;
+    ofstream file;           /*ofstraem is used to create and write information to file */
+    file.open("student.txt");
+    
+    cout<<"Enter your name:";
+    getline(cin,name);
+    
+    file<<"Welcome"<<name<<endl;
+    file.close();
+    
+    return 0;
+}
+//.............................................................................................................................................
+
+#include<iostraem>
+#include<fstream>         /*to read and write in to a file we need  <fstream> library.
+                                     */
+using namespace std;
+int main (){
+    
+    string name;
+    ofstream file;           /*ofstraem is used to create and write information to file */
+    file.open("student.txt",ios::out|ios::app);    /*if i want to add a new name then i have to write      file.open("student.txt",ios::out|ios::app); */
+    
+    cout<<"Enter your name:";
+    getline(cin,name);
+    
+    file<<"Welcome"<<name<<endl;
+    file.close();
+    
+    return 0;
+}
+
+
+
+
